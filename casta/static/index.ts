e17906574fc0bc606885ebc80ed96b3771884bd5
Esbjörn Stenberg <me@stagrim.com>
2023-02-17 11:28:50 +0100

    websocket reconnects to casta, updated disconnect img

--- casta/static/index.ts
+++ casta/static/index.ts
@@ -1,73 +1,77 @@
+interface Payload {
+    Display?: Content<Website>,
+    Disconnected?: any,
+}
+
+interface Content<T> {
+    type: "Website",
+    data: T
+}
+
+interface Website {
+    content: string
+}
+
 window.onload = () => {
-    interface Payload {
-        Display?: Content<Website>,
-        Disconnected?: any,
-    }
+    const website: HTMLIFrameElement = document.getElementById("website") as HTMLIFrameElement
 
-    interface Content<T> {
-        type: "Website",
-        data: T
-    }
+    const image: HTMLImageElement = document.getElementById("image") as HTMLImageElement
+    const background_audio: HTMLIFrameElement = document.getElementById("background_audio") as HTMLIFrameElement
 
-    interface Website {
-        content: string
-    }
-
-    const website = document.getElementById("website")
-    const website2 = document.getElementById("website2")
-    let first_website = true
-
-    const image = document.getElementById("image")
-    const background_audio = document.getElementById("background_audio")
-    let current_background_audio: string = "";
-
-    let socket = new WebSocket(`ws://${location.host}/ws`)
+    // Ignore error, library is defined in index.html
+    // TODO: import correctly?
+    // let socket = new WebSocket(`ws://${location.host}/ws`)
+    let socket = new ReconnectingWebSocket(`ws://${location.host}/ws`)
+    
 
     socket.addEventListener("open", () => {
         console.log("connected to socket")
     })
 
-    socket.addEventListener("ping", () => {
-        console.log("Received ping from server")
-    })
-    //TODO: make sure it tires to reconnect infinitely on close so refreshing page manually is not necessary
+    const assign = (element: HTMLElement | null, src?: string) => element?.setAttribute("src", src ?? "")
+
+    const display_image = (src: string) => {
+        website?.classList.add("hidden")
+        image?.classList.remove("hidden")
+        assign(image, src)
+    }
+
+    const display_website = (href: string) => {
+        console.log("Website with data: " +href +" received")
+        image.classList.add("hidden")
+        website.classList.remove("hidden")
+        assign(website, href)
+    }
+
+    const display_disconnected_sasta = () => {
+        display_image("/disconnected")
+    }
+
+    const display_disconnected_casta = () => {
+        display_image("/disconnected")
+    }
+
     socket.addEventListener("close", () => {
+        display_disconnected_casta()
         console.log("disconnected to socket")
     })
 
-    const assign = (element: HTMLElement, src?: string) => element.setAttribute("src", src ?? "")
-
-    socket.addEventListener('message', (event) => {
+    socket.addEventListener('message', (event: MessageEvent<any>) => {
         console.log(`[Debug] ${event.data}`)
         let payload: Payload = JSON.parse(event.data)
         
-        if (website && background_audio && image) {
-            if (payload.Disconnected) {
-                console.log("Now it is Disconnected")
-
-                website.classList.add("hidden")
-                image.classList.remove("hidden")
-                assign(image, "/disconnected")
-
-            } else if (payload.Display) {
-                const type = payload.Display.type
-                if (type === "Website") {
-                    const content = payload.Display.data.content
-                    console.log("Website with data: " +content +" received")
-
-                    image.classList.add("hidden")
-                    website.classList.remove("hidden")
-
-                    assign(website, content)
-                }
+        if (payload.Disconnected) {
+            display_disconnected_sasta()
+        } else if (payload.Display) {
+            const type = payload.Display.type
+            if (type === "Website") {
+                display_website(payload.Display.data.content)
             }
+        }
 
             // if (data.background_audio != undefined && data.background_audio !== current_background_audio) {
             //     current_background_audio = data.background_audio
             //     assign(background_audio, data.background_audio)
             // }
-        } else {
-            console.error("Document did not load correctly");
-        }
     });
 }
