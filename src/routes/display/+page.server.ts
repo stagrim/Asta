23deb74ae25235233932961a6071d3c1229d24ed
Esbjörn Stenberg <me@stagrim.com>
2023-06-24 21:27:13 +0200

    Use generated API types in display operations

--- src/routes/display/+page.server.ts
+++ src/routes/display/+page.server.ts
@@ -1,30 +1,39 @@
 import { SERVER_URL } from "$env/static/private"
 import { fail, type Actions, redirect } from "@sveltejs/kit"
-import type { Display, Payload } from "../../app"
+import type { CreateDisplay } from "../../api_bindings/create/CreateDisplay"
+import type { Payload } from "../../api_bindings/read/Payload"
 
 export const actions = {
     create: async ({ request }) => {
-        const data = await request.formData()
-
+        const data = await request.formData();
+        
         const name = data.get("name")
         const schedule = data.get("schedule")
+        if (!name || !schedule) {
+            return fail(400, { message: "Fields were empty" })
+        }
+        const body: CreateDisplay = {
+            name: name.toString(),
+            schedule: schedule.toString()
+        }
 
         const res = await fetch(`${SERVER_URL}/api/display`, {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ name, schedule })
+            body: JSON.stringify(body)
         })
 
-        if (res.ok) {
-            const json: Payload<Display> = await res.json()
-            throw redirect(303, `/display/${json.content[0].uuid}`);
-        } else {
-            const text = await res.text()
-            try {
-                return fail(400, { message: JSON.parse(text).message })
-            } catch {
-                return fail(400, { message: text })
+        const text = await res.text()
+        try {
+            const payload: Payload = JSON.parse(text)
+            if (payload.type == "Display") {
+                console.log(payload)
+                throw redirect(303, `/display/${payload.content[0].uuid}`);
+            } else if (payload.type == "Error") {
+                return fail(400, { message: payload.content.message })
             }
+        } catch {
+            return fail(400, { message: text })
         }
     }
 } satisfies Actions;
--- src/routes/display/[uuid]/+page.server.ts
+++ src/routes/display/[uuid]/+page.server.ts
@@ -1,11 +1,13 @@
 import { SERVER_URL } from "$env/static/private"
 import { redirect, type Actions, fail } from "@sveltejs/kit"
+import type { UpdateDisplay } from "../../../api_bindings/update/UpdateDisplay"
+import type { Payload } from "../../../api_bindings/read/Payload"
 
 export const actions = {
     delete: async ({ params }) => {
         const uuid = params.uuid
         console.log(uuid)
-        const test = await fetch(`${SERVER_URL}/api/display/${uuid}`, {
+        const _ = await fetch(`${SERVER_URL}/api/display/${uuid}`, {
             method: "DELETE",
         })
 
@@ -14,25 +16,33 @@ export const actions = {
     update: async ({ params, request }) => {
         const data = await request.formData();
         const uuid = params.uuid
-
+        
         const name = data.get("name")
         const schedule = data.get("schedule")
+        if (!name || !schedule) {
+            return fail(400, { message: "Fields were empty" })
+        }
+        const body: UpdateDisplay = {
+            name: name.toString(),
+            schedule: schedule.toString()
+        }
 
         const res = await fetch(`${SERVER_URL}/api/display/${uuid}`, {
             method: "PUT",
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ name, schedule })
+            body: JSON.stringify(body)
         })
 
-        if (res.ok) {
-            console.log(await res.json())
-        } else {
-            const text = await res.text()
-            try {
-                return fail(400, { message: JSON.parse(text).message })
-            } catch {
-                return fail(400, { message: text })
+        const text = await res.text()
+        try {
+            const payload: Payload = JSON.parse(text)
+            if (payload.type == "Display") {
+                console.log(payload)
+            } else if (payload.type == "Error") {
+                return fail(400, { message: payload.content.message })
             }
+        } catch {
+            return fail(400, { message: text })
         }
     } 
 } satisfies Actions;
