87c52879562bd275dba43f83e20e1f46fec503d2
Esbjörn Stenberg <me@stagrim.com>
2025-04-29 12:29:55 +0200

    bump dependencies, redis reconnect and gives proper error

--- sasta/src/main.rs
+++ sasta/src/main.rs
@@ -401,7 +401,12 @@ async fn create_display(
     };
     info!("[Api] Using Uuid {uuid} for new Display");
 
-    store.create_display(uuid, disp.name, disp.schedule).await;
+    if let Err(e) = store.create_display(uuid, disp.name, disp.schedule).await {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
 
     return if let Some(d) = store.read().await.displays.get(&uuid) {
         info!("[Api] Created Display {uuid}");
@@ -445,7 +450,13 @@ async fn create_playlist(
     drop(read);
     let uuid = Uuid::new_v4();
     info!("[Api] Generated Uuid {uuid} for new Playlist");
-    store.create_playlist(uuid, playlist.name).await;
+
+    if let Err(e) = store.create_playlist(uuid, playlist.name).await {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
 
     return if let Some(p) = store.read().await.playlists.get(&uuid) {
         info!("[Api] Created Playlist {uuid}");
@@ -488,9 +499,16 @@ async fn create_schedule(
     drop(read);
     let uuid = Uuid::new_v4();
     info!("[Api] Generated Uuid {uuid} for new Schedule");
-    store
+
+    if let Err(e) = store
         .create_schedule(uuid, schedule.name, schedule.playlist)
-        .await;
+        .await
+    {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
 
     return if let Some(s) = store.read().await.schedules.get(&uuid) {
         info!("[Api] Created Schedule {uuid}");
@@ -674,9 +692,15 @@ async fn update_display(
     }
     drop(read);
 
-    store
+    if let Err(e) = store
         .update_display(uuid, display.name, display.schedule)
-        .await;
+        .await
+    {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
 
     return if let Some(d) = store.read().await.displays.get(&uuid) {
         info!("[Api] Updated and read Display {uuid}");
@@ -736,9 +760,15 @@ async fn update_playlist(
 
     drop(read);
 
-    store
+    if let Err(e) = store
         .update_playlist(uuid, playlist.name, playlist.items)
-        .await;
+        .await
+    {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
 
     return if let Some(p) = store.read().await.playlists.get(&uuid) {
         info!("[Api] Updated and read Playlist {uuid}");
@@ -882,7 +912,13 @@ async fn delete_display(State(state): State<AppState>, Path(uuid): Path<Uuid>) -
         ));
     }
 
-    store.delete_display(uuid).await;
+    if let Err(e) = store.delete_display(uuid).await {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
+
     info!("[Api] Deleted Display {uuid}");
     res
 }
@@ -968,7 +1004,13 @@ pub(crate) async fn delete_playlist(
 
     drop(read);
 
-    store.delete_playlist(uuid).await;
+    if let Err(e) = store.delete_playlist(uuid).await {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
+
     info!("[Api] Deleted Playlist {uuid}");
     res
 }
@@ -1051,7 +1093,13 @@ async fn delete_schedule(State(state): State<AppState>, Path(uuid): Path<Uuid>) 
 
     drop(read);
 
-    store.delete_schedule(uuid).await;
+    if let Err(e) = store.delete_schedule(uuid).await {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json((5, format!("Could not write changes to db ({e})")).into()),
+        ));
+    }
+
     info!("[Api] Deleted Schedule {uuid}");
     res
 }
--- sasta/src/store/store.rs
+++ sasta/src/store/store.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use chrono::Local;
-use redis::{aio::MultiplexedConnection, Client, JsonAsyncCommands};
+use redis::{aio::ConnectionManager, Client, JsonAsyncCommands, RedisError};
 use serde::{Deserialize, Serialize};
 use tokio::{
     sync::{
@@ -73,7 +73,7 @@ pub struct Content {
 }
 
 pub struct Store {
-    con: Mutex<MultiplexedConnection>,
+    con: Mutex<ConnectionManager>,
     sender: Sender<Change>,
     content: RwLock<Content>,
 }
@@ -81,7 +81,7 @@ pub struct Store {
 impl Store {
     pub async fn new(redis_url: &str) -> Self {
         let client = Client::open(redis_url).unwrap();
-        let mut con = client.get_multiplexed_tokio_connection().await.unwrap();
+        let mut con = ConnectionManager::new(client).await.unwrap();
         let (sender, _) = broadcast::channel(5);
         let content = RwLock::new(Self::read_file(&mut con).await);
 
@@ -93,7 +93,7 @@ impl Store {
         s
     }
 
-    async fn read_file(con: &mut MultiplexedConnection) -> Content {
+    async fn read_file(con: &mut ConnectionManager) -> Content {
         match con.json_get::<_, _, String>("content", ".").await {
             Ok(str) => {
                 // println!("{}", str);
@@ -111,7 +111,10 @@ impl Store {
     }
 
     /// Updates all Schedules' Playlists to the (schedule_uuid, active_playlist_uuid) pairs
-    async fn update_schedule_active_playlist(&self, vec: Vec<(Uuid, Uuid)>) {
+    async fn update_schedule_active_playlist(
+        &self,
+        vec: Vec<(Uuid, Uuid)>,
+    ) -> Result<(), RedisError> {
         // Exit if schedule does not exists or if it is already set to the given playlist
         // if !self.read().await.schedules.contains_key(&schedule) || self.read().await.schedules.get(&schedule).unwrap().playlist == active_playlist {
         //     return;
@@ -127,7 +130,7 @@ impl Store {
                 vec.iter().map(|v| v.0),
             )))
         })
-        .await;
+        .await
     }
 
     /// Starts scheduling loop updating the active playlists when necessary
@@ -147,16 +150,20 @@ impl Store {
 
         // Updates all schedules to their current active scheduled playlist
         if !schedules.is_empty() {
-            self.write(|mut c| {
-                schedules.iter().for_each(|(uuid, schedule)| {
-                    c.schedules
-                        .entry(*uuid)
-                        .and_modify(|s| s.playlist = schedule.current_playlist(&current_moment));
-                });
-                // Change notice not needed since main thread waits on oneshot notice before continuing
-                None
-            })
-            .await;
+            // Does not care about redis error, since only changes internal state.
+            // TODO: Make new method which only changes internal state and does not write
+            // to db to make this clearer?
+            let _ = self
+                .write(|mut c| {
+                    schedules.iter().for_each(|(uuid, schedule)| {
+                        c.schedules.entry(*uuid).and_modify(|s| {
+                            s.playlist = schedule.current_playlist(&current_moment)
+                        });
+                    });
+                    // Change notice not needed since main thread waits on oneshot notice before continuing
+                    None
+                })
+                .await;
             info!("[Scheduler] Updated Schedules to current active playlist");
         }
 
@@ -236,7 +243,7 @@ impl Store {
                             //TODO: When updating a schedule, the new schedule is overridden in the API, and since the 'set current block' lies before the loop, they are never reverted to the present version
                             Ok(Change::ScheduleInput(uuids)) => {
                                 info!("[Scheduler] Schedules updated, rerunning loop");
-                                self.write(|mut c| {
+                                let _ = self.write(|mut c| {
                                     uuids.iter().for_each(|uuid| {
                                         c.schedules
                                             .entry(*uuid)
@@ -254,16 +261,17 @@ impl Store {
             }
             info!("[Scheduler] Sleep done, updating active playlists");
 
-            self.update_schedule_active_playlist(
-                moments
-                    .iter()
-                    .map(|(u, m)| (*u, m.playlist))
-                    .inspect(|(uuid, _)| {
-                        info!("[Scheduler] Updating Schedule {uuid} active playlist")
-                    })
-                    .collect::<Vec<_>>(),
-            )
-            .await;
+            let _ = self
+                .update_schedule_active_playlist(
+                    moments
+                        .iter()
+                        .map(|(u, m)| (*u, m.playlist))
+                        .inspect(|(uuid, _)| {
+                            info!("[Scheduler] Updating Schedule {uuid} active playlist")
+                        })
+                        .collect::<Vec<_>>(),
+                )
+                .await;
             current_moment = closest_time;
         }
     }
@@ -279,7 +287,7 @@ impl Store {
 
     /// Runs closure with lock write guard handle given as argument
     /// and sends a message signalling a state change once it is done
-    async fn write<F>(&self, fun: F)
+    async fn write<F>(&self, fun: F) -> Result<(), RedisError>
     where
         F: FnOnce(RwLockWriteGuard<Content>) -> Option<Change>,
     {
@@ -299,25 +307,32 @@ impl Store {
             .await
         {
             error_span!("Redis Error", ?error);
-            // error_span!("Logging current state instead", ?self.content);
+            Err(error)
+        } else {
+            Ok(())
         }
     }
 
     /// Creates a new display
     ///
     /// Overrides existing display with same uuid
-    pub async fn create_display(&self, uuid: Uuid, name: String, schedule: Uuid) {
+    pub async fn create_display(
+        &self,
+        uuid: Uuid,
+        name: String,
+        schedule: Uuid,
+    ) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.displays.insert(uuid, Display { name, schedule });
             Some(Change::Display(HashSet::from([uuid])))
         })
-        .await;
+        .await
     }
 
     /// Creates a new playlist
     ///
     /// Overrides existing playlist with same uuid
-    pub async fn create_playlist(&self, uuid: Uuid, name: String) {
+    pub async fn create_playlist(&self, uuid: Uuid, name: String) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.playlists.insert(
                 uuid,
@@ -328,25 +343,35 @@ impl Store {
             );
             Some(Change::Playlist(HashSet::from([uuid])))
         })
-        .await;
+        .await
     }
 
     /// Creates a new Schedule
     ///
     /// Overrides existing Schedule with same uuid
-    pub async fn create_schedule(&self, uuid: Uuid, name: String, playlist: Uuid) {
+    pub async fn create_schedule(
+        &self,
+        uuid: Uuid,
+        name: String,
+        playlist: Uuid,
+    ) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.schedules
                 .insert(uuid, Schedule::new(name, vec![], playlist).unwrap());
             Some(Change::Schedule(HashSet::from([uuid])))
         })
-        .await;
+        .await
     }
 
     /// Updates the display with the given Uuid
     ///
     /// Does nothing if no such display is found
-    pub async fn update_display(&self, uuid: Uuid, name: String, schedule: Uuid) {
+    pub async fn update_display(
+        &self,
+        uuid: Uuid,
+        name: String,
+        schedule: Uuid,
+    ) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.displays
                 .entry(uuid)
@@ -359,7 +384,12 @@ impl Store {
     /// Updates the playlist with the given Uuid
     ///
     /// Does nothing if no such playlist is found
-    pub async fn update_playlist(&self, uuid: Uuid, name: String, items: Vec<PlaylistItem>) {
+    pub async fn update_playlist(
+        &self,
+        uuid: Uuid,
+        name: String,
+        items: Vec<PlaylistItem>,
+    ) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.playlists
                 .entry(uuid)
@@ -387,14 +417,14 @@ impl Store {
             c.schedules.entry(uuid).and_modify(|s| *s = schedule);
             Some(Change::ScheduleInput(HashSet::from([uuid])))
         })
-        .await;
-        Ok(())
+        .await
+        .map_err(|e| e.to_string())
     }
 
     /// Deletes the display with the given Uuid
     ///
     /// Does nothing if no such display is found
-    pub async fn delete_display(&self, uuid: Uuid) {
+    pub async fn delete_display(&self, uuid: Uuid) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.displays.remove(&uuid);
             Some(Change::Display(HashSet::from([uuid])))
@@ -405,7 +435,7 @@ impl Store {
     /// Deletes the Playlist with the given Uuid
     ///
     /// Does nothing if no such Playlist is found
-    pub async fn delete_playlist(&self, uuid: Uuid) {
+    pub async fn delete_playlist(&self, uuid: Uuid) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.playlists.remove(&uuid);
             Some(Change::Playlist(HashSet::from([uuid])))
@@ -416,7 +446,7 @@ impl Store {
     /// Deletes the Schedule with the given Uuid
     ///
     /// Does nothing if no such Schedule is found
-    pub async fn delete_schedule(&self, uuid: Uuid) {
+    pub async fn delete_schedule(&self, uuid: Uuid) -> Result<(), RedisError> {
         self.write(|mut c| {
             c.schedules.remove(&uuid);
             Some(Change::Schedule(HashSet::from([uuid])))
