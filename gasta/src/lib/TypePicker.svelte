fcff6afafa6f13e0dff1919983702f4e756259bf
Esbjörn Stenberg <me@stagrim.com>
2025-11-09 12:51:29 +0100

    Started redesigning new display

--- gasta/src/lib/TypePicker.svelte
+++ gasta/src/lib/TypePicker.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
 	import arrowRight from 'svelte-awesome/icons/arrowRight';
+	import * as Select from '$lib/components/ui/select';
+	import * as InputGroup from '$lib/components/ui/input-group';
 
 	import { Icon } from 'svelte-awesome';
 	import type { State } from '../app';
+	import { ArrowRight, SearchIcon } from '@lucide/svelte';
+	import InputGroupSelectTrigger from './components/ui/InputGroupSelectTrigger.svelte';
 
-	export let types: State;
-	export let label = true;
+	let {
+		types,
+		label = true,
+		// BUG: Should always have a valid uuid
+		chosen_type = $bindable(''),
+		name = ''
+	}: {
+		types: State;
+		label?: boolean;
+		/** Bind value to react to user changes */
 
-	/** Bind value to react to user changes */
-	export let chosen_type: string = '';
+		chosen_type?: string;
+		/** Name property of the select element for form */
+		name?: string;
+	} = $props();
 
-	$: types_values = [...types.content.values()].sort((a, b) => a.name.localeCompare(b.name));
-
-	/** Name property of the select element for form */
-	export let name = '';
+	let types_values = $derived(
+		[...types.content.values()].sort((a, b) => a.name.localeCompare(b.name))
+	);
 </script>
 
 <label class="label">
@@ -22,7 +35,7 @@
 	{/if}
 
 	<div class="flex flex-row items-center input-group input-group-divider cursor-pointer">
-		<select
+		<!-- <select
 			required
 			{name}
 			class="select"
@@ -32,14 +45,39 @@
 			{#each types_values as type (type.uuid)}
 				<option value={type.uuid}>{type.name}</option>
 			{/each}
-		</select>
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<a class="h-[40px] w-10" href={`/${types.type.toLocaleLowerCase()}/${chosen_type}`}>
+		</select> -->
+
+		<InputGroup.Root>
+			<Select.Root required {name} bind:value={chosen_type} type="single">
+				<InputGroupSelectTrigger class="w-full"
+					>{types_values.find((v) => v.uuid == chosen_type)?.name}</InputGroupSelectTrigger
+				>
+				<Select.Content>
+					<Select.Label>{types.type}s</Select.Label>
+					{#each types_values as type (type.uuid)}
+						<Select.Item value={type.uuid}>{type.name}</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
+			<InputGroup.Addon align="inline-end">
+				{#if chosen_type !== '0'}
+					<a href={`/${types.type.toLocaleLowerCase()}/${chosen_type}`}>
+						<InputGroup.Button variant="secondary"><ArrowRight /></InputGroup.Button>
+					</a>
+				{/if}
+				<!-- <a href={`/${types.type.toLocaleLowerCase()}/${chosen_type}`}>
+					<div>
+						<ArrowRight />
+					</div>
+				</a> -->
+			</InputGroup.Addon>
+		</InputGroup.Root>
+		<!-- style="min-width: unset !important;" -->
+
+		<!-- <a class="h-[40px] w-10" href={`/${types.type.toLocaleLowerCase()}/${chosen_type}`}>
 			<div>
-				<!-- <button type="button"> -->
 				<Icon data={arrowRight} scale={0.75} />
-				<!-- </button> -->
 			</div>
-		</a>
+		</a> -->
 	</div>
 </label>
--- gasta/src/routes/(model)/display/+page.svelte
+++ gasta/src/routes/(model)/display/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import * as Card from '$lib/components/ui/card';
 	import { enhance } from '$app/forms';
 	import { form_action } from '$lib/form_action';
 	import TypePicker from '$lib/TypePicker.svelte';
 	import type { PageData } from './$types';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import * as Select from '$lib/components/ui/select/index.js';
 
 	let { data }: { data: PageData } = $props();
 
@@ -19,7 +23,42 @@
 		return form_action;
 	}}
 >
-	<section class="p-4">
+	<Card.Root>
+		<Card.Content>
+			<div class="grid gap-2 mb-5">
+				<Label>Uuid</Label>
+				<Input name="uuid" class="input" type="text" placeholder="Uuid for display (optional)" />
+			</div>
+
+			<div class="grid gap-2 mb-5">
+				<Label>Name</Label>
+				<Input required name="name" class="input" type="text" placeholder="Name must be unique" />
+			</div>
+
+			<h1>Display Material</h1>
+
+			<div class="flex md:items-center w-full gap-4 md:flex-row flex-col">
+				<Select.Root name="display_type" type="single" bind:value={type}>
+					<Select.Trigger class="w-[180px]"
+						>{type.charAt(0).toUpperCase()}{type.substring(1)}</Select.Trigger
+					>
+					<Select.Content>
+						<Select.Label>Types</Select.Label>
+						<Select.Item value="schedule">Schedule</Select.Item>
+						<Select.Item value="playlist">Playlist</Select.Item>
+					</Select.Content>
+				</Select.Root>
+				<div class="w-full">
+					{#if type === 'schedule'}
+						<TypePicker label={false} types={data.schedule} bind:chosen_type={chosen_schedule} />
+					{:else if type === 'playlist'}
+						<TypePicker label={false} types={data.playlist} bind:chosen_type={chosen_playlist} />
+					{/if}
+				</div>
+			</div>
+		</Card.Content>
+	</Card.Root>
+	<!-- <section class="p-4">
 		<label class="label mb-5">
 			<span>Uuid</span>
 			<input name="uuid" class="input" type="text" placeholder="Uuid for display (optional)" />
@@ -54,8 +93,7 @@
 		/>
 
 		<div class="flex w-full justify-center mt-5">
-			<!-- Disable button when nothing is changed -->
 			<button type="submit" class="btn variant-filled-primary">Add</button>
 		</div>
-	</section>
+	</section> -->
 </form>
